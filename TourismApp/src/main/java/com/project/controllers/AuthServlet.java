b9fc4ebe6bf9217be660665f126b60a00c345013
Tharusha Akash <tharushaakasha22@gmail.com>
2025-04-08 19:25:50 +0530

    UP

--- TourismApp/src/main/java/com/project/controllers/AuthServlet.java
+++ TourismApp/src/main/java/com/project/controllers/AuthServlet.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 @WebServlet(name = "AuthServlet", value = "/login")
 public class AuthServlet extends HttpServlet {
+    private static final String USERS_FILE = "users.txt";
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
@@ -15,10 +16,15 @@ public class AuthServlet extends HttpServlet {
 
         String file = role.equals("admin") ? "admins.txt" : "users.txt";
         String[] records = FileHandler.readFromFile(file);
+        String files = USERS_FILE;
+
 
         for (String record : records) {
-            String[] parts = record.split(",");
-            if (parts.length == 2 && parts[0].equals(email) && parts[1].equals(password)) {
+            String[] parts = record.split(","); //Split in to 2 parts (String = [email, password])
+
+            //Check part1 == user submitted email and part2 == password
+            if (parts.length == 2 && parts[0].equals(email) && parts[1].equals(password)) {  //check actually it has 2 parts
+                //create a new session
                 HttpSession session = request.getSession();
                 session.setAttribute("email", email);
                 session.setAttribute("role", role);
@@ -33,6 +39,6 @@ public class AuthServlet extends HttpServlet {
             }
         }
 
-        response.getWriter().println("Invalid login. <a href='login.jsp'>Try again</a>");
+        response.sendRedirect("login.jsp?error=Invalid email or password");
     }
 }
